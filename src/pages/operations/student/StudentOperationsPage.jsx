c9dbd05397bbb52fbb7c311c40a7c5e1ae913bc6
0Armaan025 <armaan33000@gmail.com>
2024-03-23 13:56:51 +0530

    static operations for students done

--- src/pages/operations/student/StudentOperationsPage.jsx
+++ src/pages/operations/student/StudentOperationsPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../../components/navbar/Navbar";
+import "./studentoperationspage.css";
 import LeftSideBar from "../../../components/leftsidebar/LeftSideBar";
 
 const StudentOperationsPage = () => {
@@ -9,11 +10,39 @@ const StudentOperationsPage = () => {
       <div className="studentOperationsPage flex flex-row">
         <LeftSideBar />
         <div className="rightSide flex flex-col pl-64">
+          <h4
+            className="text-3xl mr-16 underline"
+            style={{ fontFamily: "Poppins" }}
+          >
+            {" "}
+            Operations{" "}
+          </h4>
           <div className="row flex flex-row justify-center items-center">
-            <div className="component px-6 py-4 ">
-              {/* dw, same here */}
-              <h2> AI FLASH CARDS</h2>
-              {/* thank you so much! */}
+            <div className="component mt-8 px-8 py-8  transition-all hover:cursor-pointer hover:border-black border-1 bg-[#828282] text-center items-center justify-center flex flex-col rounded-lg hover:bg-[#605f5f]">
+              <h4
+                className="text-2xl text-white font-semibold"
+                style={{ background: "none" }}
+              >
+                AI FLASHCARDS
+              </h4>
+            </div>
+            <div className="component ml-6 mt-8 px-8 py-8  transition-all hover:cursor-pointer hover:border-black border-1 bg-[#828282] text-center items-center justify-center flex flex-col rounded-lg hover:bg-[#605f5f]">
+              <h4
+                className="text-2xl text-white font-semibold"
+                style={{ background: "none" }}
+              >
+                AI NOTES/SUMMARIES
+              </h4>
+            </div>
+          </div>
+          <div className="row flex flex-row justify-start items-start">
+            <div className="component mt-8 px-8 py-8  transition-all hover:cursor-pointer hover:border-black border-1 bg-[#828282] text-center items-start justify-start flex flex-col rounded-lg hover:bg-[#605f5f]">
+              <h4
+                className="text-2xl text-white font-semibold"
+                style={{ background: "none" }}
+              >
+                AI MENTOR
+              </h4>
             </div>
           </div>
         </div>
