a11a7289f03db9ae514e023645a792684732502b
0Armaan025 <armaan33000@gmail.com>
2024-03-23 11:35:27 +0530

    setup profile for students done

--- src/pages/profile-set-up/ProfileSetupPage.jsx
+++ src/pages/profile-set-up/ProfileSetupPage.jsx
@@ -9,7 +9,7 @@ const ProfileSetupPage = () => {
       <div className="profileSetupPage flex flex-col justify-center items-center">
         <center>
           <h3
-            className="mt-8 text-4xl font-bold"
+            className="mt-8 text-4xl font-bold text-#33272a]"
             style={{ fontFamily: "Poppins" }}
           >
             Let's Setup your Profile! 🥳
@@ -18,7 +18,7 @@ const ProfileSetupPage = () => {
           <br />
           <div className="detailsField flex flex-col justify-start items-start bg-[#dadada] p-8 rounded-lg ">
             <label
-              className="text-xl"
+              className="text-xl text-[#33272a]"
               style={{
                 fontFamily: "lato",
                 background: "none",
@@ -34,7 +34,7 @@ const ProfileSetupPage = () => {
             />
             <br />
             <label
-              className="text-xl"
+              className="text-xl text-[#33272a]"
               style={{
                 fontFamily: "lato",
                 background: "none",
@@ -50,7 +50,7 @@ const ProfileSetupPage = () => {
             />
             <br />
             <label
-              className="text-xl"
+              className="text-xl text-[#33272a]"
               style={{
                 fontFamily: "lato",
                 background: "none",
@@ -64,6 +64,18 @@ const ProfileSetupPage = () => {
               type="text"
               className="p-2 px-2 w-[380px] mt-2 border-2 border-black rounded-lg"
             />
+            <br />
+            <div
+              className="teacherSignUpDiv flex flex-row justify-end items-end w-full bg-none"
+              style={{ background: "none" }}
+            >
+              <h4
+                style={{ background: "none", fontFamily: "Poppins" }}
+                className="text-[#33272a] hover:cursor-pointer"
+              >
+                Click here to sign up as a teacher!
+              </h4>
+            </div>
             <br />
             <div
               className="buttonDiv w-full flex flex-row justify-center items-center bg-none"
